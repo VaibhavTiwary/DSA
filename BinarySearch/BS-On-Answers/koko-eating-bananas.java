71e68373f6f75c89d1c1c547f486ff04d40aa9cf
Vaibhav <vaibhavtiwary7766@gmail.com>
2025-04-04 15:27:11 +0530

    koko eating bananas

---
+++ BinarySearch/BS-On-Answers/koko-eating-bananas.java
@@ -0,0 +1,48 @@
+// Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+// Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+// Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+// Return the minimum integer k such that she can eat all the bananas within h hours.
+
+ 
+
+// Example 1:
+
+// Input: piles = [3,6,7,11], h = 8
+// Output: 4
+// Example 2:
+
+// Input: piles = [30,11,23,4,20], h = 5
+// Output: 30
+
+
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int max = 0;
+        for(int i = 0; i < piles.length; i++){
+            max = Math.max(max, piles[i]);
+        }
+
+        int low = 1;
+        int high = max;
+
+        while(high > low){
+            int mid = low + (high - low) / 2;
+
+            int k = 0;
+
+            for(int i = 0; i < piles.length; i++){
+                k += (piles[i] + mid - 1) / mid;
+            }
+
+            if(k <= h){
+                high = mid;
+            }
+            else low = mid + 1;
+        }
+        return low;
+    }
+}
---
+++ BinarySearch/BS-On-Answers/maximum-candies-allocated-to-k-children.java
@@ -0,0 +1,57 @@
+// You are given a 0-indexed integer array candies. Each element in the array denotes a pile of candies 
+// of size candies[i]. You can divide each pile into any number of sub piles, but you cannot merge two 
+// piles together.
+
+// You are also given an integer k. You should allocate piles of candies to k children such that each 
+// child gets the same number of candies. Each child can be allocated candies from only one pile of 
+// candies and some piles of candies may go unused.
+
+// Return the maximum number of candies each child can get.
+
+ 
+
+// Example 1:
+
+// Input: candies = [5,8,6], k = 3
+// Output: 5
+// Explanation: We can divide candies[1] into 2 piles of size 5 and 3, and candies[2] into 2 piles of 
+// size 5 and 1. We now have five piles of candies of sizes 5, 5, 3, 5, and 1. We can allocate the 3 
+// piles of size 5 to 3 children. It can be proven that each child cannot receive more than 5 candies.
+
+
+
+
+
+
+
+class Solution {
+    public int maximumCandies(int[] candies, long k) {
+
+        int max = candies[0];
+        for(int i = 1; i < candies.length; i++){
+            max = Math.max(candies[i], max);
+        }
+
+        int low = 1;
+        int high = max;
+        int best = 0;
+        
+        while(high >= low){
+            int mid = (low + high) / 2;
+            long piles = 0;
+
+            for(int i = 0; i < candies.length; i++){
+                piles += (candies[i] / mid);
+            }
+
+            if(piles >= k){
+                best = mid;
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return best;
+    }
+}
